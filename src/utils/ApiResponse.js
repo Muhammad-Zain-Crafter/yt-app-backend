6ef035e9a9a1deadfa2411d069c83700e184a8e8
Muhammad-Zain-Crafter <zmunawar872@gmail.com>
2025-08-03 04:30:28 -0700

    add user and video model

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,12 @@
+class ApiResponse {
+    constructor(
+        statuCode,
+        data,
+        message = "Request was successful"
+    ){
+        this.statuCode = statuCode;
+        this.data = data;
+        this.message = message;
+        this.success = statuCode < 400;
+    }
+}
