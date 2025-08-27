fc957c1fc63141796c3be07e22d14f8c24c641cc
Muhammad-Zain-Crafter <zmunawar872@gmail.com>
2025-08-27 09:44:05 -0700

    Add subscription routes and controller; implement subscription toggle and retrieval logic

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,16 +21,15 @@ app.use(cookieParser()); // for parsing cookies
 // import routes
 import userRouter from './routes/user.route.js';
 import videoRouter from './routes/video.route.js';
-
+import likeRouter from './routes/like.route.js';
+import commentRouter from './routes/comment.route.js';
+import subscriptionRouter from './routes/subscription.route.js';
 // routes declaration
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/videos", videoRouter)
-
-
-
-
-
-
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
 
 
 
